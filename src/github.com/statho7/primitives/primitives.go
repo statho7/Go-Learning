d2a2ba8edbfa707a91bd6dfd0594e7fcd812c76d
statho7 <andreas.stathop@gmail.com>
2021-12-06 19:09:52 +0000

    calc

--- src/github.com/statho7/primitives/primitives.go
+++ src/github.com/statho7/primitives/primitives.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+    "fmt"
+)
+
+// func main() {
+// 	// n := 1 == 1
+// 	// m := 1 == 2
+// 	// fmt.Printf("%v, %T\n",n,n)
+// 	// fmt.Printf("%v, %T\n",m,m)
+
+// 	// var n bool
+// 	// fmt.Printf("%v, %T\n",n,n)
+
+// 	// var n int
+// 	// n := 42
+// 	var n uint16 = 42
+// 	fmt.Printf("%v, %T\n",n,n)
+// }
+
+func main() {
+	a := 10
+	b := 3
+	fmt.Println(a + b)
+	fmt.Println(a - b)
+	fmt.Println(a * b)
+	fmt.Println(a / b)
+	fmt.Println(a % b)
+}
