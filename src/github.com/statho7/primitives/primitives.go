dea3120a0d5f0fc3399da93ad17b2d373b59b262
statho7 <andreas.stathop@gmail.com>
2021-12-06 20:01:04 +0000

    imaginary

--- src/github.com/statho7/primitives/primitives.go
+++ src/github.com/statho7/primitives/primitives.go
@@ -67,11 +67,33 @@ import (
 // 	fmt.Printf("%v, %T\n",n,n)
 // }
 
-func main()  {
-	a := 10.2
-	b := 3.7
-	fmt.Println(a + b)
-	fmt.Println(a - b)
-	fmt.Println(a * b)
-	fmt.Println(a / b)
-}
+// func main()  {
+// 	a := 10.2
+// 	b := 3.7
+// 	fmt.Println(a + b)
+// 	fmt.Println(a - b)
+// 	fmt.Println(a * b)
+// 	fmt.Println(a / b)
+// }
+
+// imaginary
+// func main()  {
+// 	// var n complex64 = 2i
+// 	// var n complex64 = 1 + 2i
+// 	// fmt.Printf("%v, %T\n",n,n)
+// 	// a := 1 + 2i
+// 	// b := 2 + 5.2i
+// 	// fmt.Println(a + b)
+// 	// fmt.Println(a - b)
+// 	// fmt.Println(a * b)
+// 	// fmt.Println(a / b)
+		
+// 	// var n complex64 = 1 + 2i
+// 	// var n complex128 = 1 + 2i
+// 	// fmt.Printf("%v, %T\n",real(n),real(n))
+// 	// fmt.Printf("%v, %T\n",imag(n),imag(n))
+
+	
+// 	var n complex128 = complex(5,12)
+// 	fmt.Printf("%v, %T\n",n,n)
+// }
