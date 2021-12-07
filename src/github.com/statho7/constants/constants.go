08cc7448a6373ceb146476dbe32996b35f865b94
statho7 <andreas.stathop@gmail.com>
2021-12-07 00:06:52 +0000

    add 2 int consts

--- src/github.com/statho7/constants/constants.go
+++ src/github.com/statho7/constants/constants.go
@@ -5,17 +5,32 @@ import (
 	// "math"
 )
 
-func main()  {
-	// const myConst int = 42
-	// const myConst float64 = math.Sin(1.57) // Error
-	// fmt.Printf("%v, %T\n", myConst, myConst)
+// func main()  {
+// 	// const myConst int = 42
+// 	// const myConst float64 = math.Sin(1.57) // Error
+// 	// fmt.Printf("%v, %T\n", myConst, myConst)
+// }
 
-	const a int = 14
-	const b string = "foo"
-	const c float32 = 3.14
-	const d bool = true
-	fmt.Printf("%v\n", a)
-	fmt.Printf("%v\n", b)
-	fmt.Printf("%v\n", c)
-	fmt.Printf("%v\n", d)
+// func main()  {
+// 	const a int = 14
+// 	const b string = "foo"
+// 	const c float32 = 3.14
+// 	const d bool = true
+// 	fmt.Printf("%v\n", a)
+// 	fmt.Printf("%v\n", b)
+// 	fmt.Printf("%v\n", c)
+// 	fmt.Printf("%v\n", d)
+// }
+
+// const a int16 = 27
+
+// func main()  {
+// 	// const a int = 14
+// 	fmt.Printf("%v, %T\n", a, a)
+// }
+
+func main()  {
+	const a int = 42
+	const b int = 27
+	fmt.Printf("%v, %T\n", a + b, a + b)
 }
