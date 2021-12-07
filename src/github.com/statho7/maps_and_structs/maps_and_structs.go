5e1d280cf756a134ddf8990e7c40bdc2e04d78dc
statho7 <andreas.stathop@gmail.com>
2021-12-07 21:50:50 +0000

    structs go example

--- src/github.com/statho7/maps_and_structs/maps_and_structs.go
+++ src/github.com/statho7/maps_and_structs/maps_and_structs.go
@@ -4,58 +4,93 @@ import(
 	"fmt"
 )
 
-func main()  {
-	// statePopulations := make(map[string]int)
+// func main()  {
+// 	// statePopulations := make(map[string]int)
 
-	// statePopulations = map[string]int{
-	// 	"California":		39250017,
-	// 	"Texas":			27862596,
-	// 	"Florida":			20612439,
-	// 	"New York":			19745286,
-	// 	"Pennsylvania":		12802503,
-	// 	"Illinois":			12801539,
-	// 	"Ohio":				11614373,
-	// }
-	
-	// m := map[[3]int]string{}
-	// fmt.Println(statePopulations, m)
-	// fmt.Println(statePopulations)
-	// fmt.Println(statePopulations["Ohio"])
-	// statePopulations["Georgia"] = 10310371
-	// delete(statePopulations, "Georgia")
-	// fmt.Println(statePopulations)
-	// fmt.Println(statePopulations["Georgia"])
+// 	// statePopulations = map[string]int{
+// 	// 	"California":		39250017,
+// 	// 	"Texas":			27862596,
+// 	// 	"Florida":			20612439,
+// 	// 	"New York":			19745286,
+// 	// 	"Pennsylvania":		12802503,
+// 	// 	"Illinois":			12801539,
+// 	// 	"Ohio":				11614373,
+// 	// }
 
-	// pop, ok := statePopulations["Oho"]
-	// fmt.Println(pop, ok)
-	// pop, ok = statePopulations["Ohio"]
-	// fmt.Println(pop, ok)
+// 	// m := map[[3]int]string{}
+// 	// fmt.Println(statePopulations, m)
+// 	// fmt.Println(statePopulations)
+// 	// fmt.Println(statePopulations["Ohio"])
+// 	// statePopulations["Georgia"] = 10310371
+// 	// delete(statePopulations, "Georgia")
+// 	// fmt.Println(statePopulations)
+// 	// fmt.Println(statePopulations["Georgia"])
 
-	// fmt.Println(len(statePopulations))
+// 	// pop, ok := statePopulations["Oho"]
+// 	// fmt.Println(pop, ok)
+// 	// pop, ok = statePopulations["Ohio"]
+// 	// fmt.Println(pop, ok)
 
-	// sp := statePopulations
-	// delete(sp, "Ohio")
-	// fmt.Println(sp)
-	// fmt.Println(statePopulations)
+// 	// fmt.Println(len(statePopulations))
 
-    m := make(map[string]int)
+// 	// sp := statePopulations
+// 	// delete(sp, "Ohio")
+// 	// fmt.Println(sp)
+// 	// fmt.Println(statePopulations)
 
-    m["k1"] = 7
-    m["k2"] = 13
+//     // m := make(map[string]int)
 
-    fmt.Println("map:", m)
+//     // m["k1"] = 7
+//     // m["k2"] = 13
 
-    v1 := m["k1"]
-    fmt.Println("v1: ", v1)
+//     // fmt.Println("map:", m)
 
-    fmt.Println("len:", len(m))
+//     // v1 := m["k1"]
+//     // fmt.Println("v1: ", v1)
 
-    delete(m, "k2")
-    fmt.Println("map:", m)
+//     // fmt.Println("len:", len(m))
 
-    _, prs := m["k2"]
-    fmt.Println("prs:", prs)
+//     // delete(m, "k2")
+//     // fmt.Println("map:", m)
 
-    n := map[string]int{"foo": 1, "bar": 2}
-    fmt.Println("map:", n)
+//     // _, prs := m["k2"]
+//     // fmt.Println("prs:", prs)
+
+//     // n := map[string]int{"foo": 1, "bar": 2}
+//     // fmt.Println("map:", n)
+// }
+
+// Structs
+type person struct {
+    name string
+    age  int
+}
+
+func newPerson(name string) *person {
+
+    p := person{name: name}
+    p.age = 42
+    return &p
+}
+
+func main() {
+
+    fmt.Println(person{"Bob", 20})
+
+    fmt.Println(person{name: "Alice", age: 30})
+
+    fmt.Println(person{name: "Fred"})
+
+    fmt.Println(&person{name: "Ann", age: 40})
+
+    fmt.Println(newPerson("Jon"))
+
+    s := person{name: "Sean", age: 50}
+    fmt.Println(s.name)
+
+    sp := &s
+    fmt.Println(sp.age)
+
+    sp.age = 51
+    fmt.Println(sp.age)
 }
