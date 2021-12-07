0ce991769f27455878beaf1c98741839144788f1
statho7 <andreas.stathop@gmail.com>
2021-12-07 21:49:28 +0000

    maps

--- src/github.com/statho7/maps_and_structs/maps_and_structs.go
+++ src/github.com/statho7/maps_and_structs/maps_and_structs.go
@@ -5,17 +5,18 @@ import(
 )
 
 func main()  {
-	statePopulations := make(map[string]int)
+	// statePopulations := make(map[string]int)
 
-	statePopulations = map[string]int{
-		"California":		39250017,
-		"Texas":			27862596,
-		"Florida":			20612439,
-		"New York":			19745286,
-		"Pennsylvania":		12802503,
-		"Illinois":			12801539,
-		"Ohio":				11614373,
-	}
+	// statePopulations = map[string]int{
+	// 	"California":		39250017,
+	// 	"Texas":			27862596,
+	// 	"Florida":			20612439,
+	// 	"New York":			19745286,
+	// 	"Pennsylvania":		12802503,
+	// 	"Illinois":			12801539,
+	// 	"Ohio":				11614373,
+	// }
+	
 	// m := map[[3]int]string{}
 	// fmt.Println(statePopulations, m)
 	// fmt.Println(statePopulations)
@@ -25,8 +26,36 @@ func main()  {
 	// fmt.Println(statePopulations)
 	// fmt.Println(statePopulations["Georgia"])
 
-	pop, ok := statePopulations["Oho"]
-	fmt.Println(pop, ok)
-	pop, ok = statePopulations["Ohio"]
-	fmt.Println(pop, ok)
+	// pop, ok := statePopulations["Oho"]
+	// fmt.Println(pop, ok)
+	// pop, ok = statePopulations["Ohio"]
+	// fmt.Println(pop, ok)
+
+	// fmt.Println(len(statePopulations))
+
+	// sp := statePopulations
+	// delete(sp, "Ohio")
+	// fmt.Println(sp)
+	// fmt.Println(statePopulations)
+
+    m := make(map[string]int)
+
+    m["k1"] = 7
+    m["k2"] = 13
+
+    fmt.Println("map:", m)
+
+    v1 := m["k1"]
+    fmt.Println("v1: ", v1)
+
+    fmt.Println("len:", len(m))
+
+    delete(m, "k2")
+    fmt.Println("map:", m)
+
+    _, prs := m["k2"]
+    fmt.Println("prs:", prs)
+
+    n := map[string]int{"foo": 1, "bar": 2}
+    fmt.Println("map:", n)
 }
