02d8011985d65353e76ebf00c0bdfd5293d89ece
statho7 <andreas.stathop@gmail.com>
2021-12-07 00:25:57 +0000

    example with holding many info in a single byte

--- src/github.com/statho7/constants/constants.go
+++ src/github.com/statho7/constants/constants.go
@@ -91,19 +91,38 @@ import (
 // 	fmt.Printf("%v\n", dogSpecialist)
 // }
 
+// const (
+// 	_ = iota // ignore first value by assigning to blank identifier
+// 	KB = 1 << (10 * iota)
+// 	MB
+// 	GB
+// 	TB
+// 	PB
+// 	EB
+// 	ZB
+// 	YB
+// )
+
+// func main()  {
+// 	fileSize := 4000000000.
+// 	fmt.Printf("%.2fGB", fileSize/GB)
+// }
+
 const (
-	_ = iota // ignore first value by assigning to blank identifier
-	KB = 1 << (10 * iota)
-	MB
-	GB
-	TB
-	PB
-	EB
-	ZB
-	YB
+	isAdmin = 1 << iota
+	isHeadquarters
+	canSeeFinancials
+
+	canSeeAfrica
+	canSeeAsia
+	canSeeEurope
+	canSeeNorthAmerica
+	canSeeSouthAmerica
 )
 
 func main()  {
-	fileSize := 4000000000.
-	fmt.Printf("%2fGB", fileSize/GB)
+	var roles byte = isAdmin | canSeeFinancials | canSeeEurope
+	fmt.Printf("%b\n", roles)
+	fmt.Printf("Is Admin ? %v\n", isAdmin & roles == isAdmin)
+	fmt.Printf("Is HQ ? %v", isHeadquarters & roles == isHeadquarters)
 }
