24f12efd57dc3d1e7b1cc34f29e8168f47aef79c
statho7 <andreas.stathop@gmail.com>
2021-12-09 23:25:51 +0000

    weird

--- src/github.com/statho7/defer_panic_recover/yolo.go
+++ src/github.com/statho7/defer_panic_recover/yolo.go
@@ -22,24 +22,34 @@ package main
 // 	defer fmt.Println("end")
 // }
 
+// import (
+// 	"fmt"
+// 	"io/ioutil"
+// 	"log"
+// 	"net/http"
+// )
+
+// func main() {
+// 	res, err := http.Get("http://www.google.com/robots.txt")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	robots, err := ioutil.ReadAll(res.Body)
+// 	defer res.Body.Close()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	fmt.Printf("%s", robots)
+// }
+
 import (
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
 )
 
 func main() {
-	res, err := http.Get("http://www.google.com/robots.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	robots, err := ioutil.ReadAll(res.Body)
-	res.Body.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Printf("%s", robots)
+	a := "start"
+	defer fmt.Println(a)
+	a = "end"
 }
