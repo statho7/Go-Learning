422a4f3ac9f354af20431d2acf150f5baef85b56
statho7 <andreas.stathop@gmail.com>
2021-12-09 22:43:31 +0000

    if and switch

--- src/github.com/statho7/if_and_switch/if_and_switch.go
+++ src/github.com/statho7/if_and_switch/if_and_switch.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 )
 
+func returnTrue() bool {
+	return true
+}
+
 func main()  {
 	// if false {
 	// 	fmt.Println("The test is true")
@@ -11,17 +15,46 @@ func main()  {
 	// 	fmt.Println("The test is yolo")
 	// }
 
-	statePopulations := map[string]int{
-		"California":		39250017,
-		"Texas":			27862596,
-		"Florida":			20612439,
-		"New York":			19745286,
-		"Pennsylvania":		12802503,
-		"Illinois":			12801539,
-		"Ohio":				11614373,
+	// statePopulations := map[string]int{
+	// 	"California":		39250017,
+	// 	"Texas":			27862596,
+	// 	"Florida":			20612439,
+	// 	"New York":			19745286,
+	// 	"Pennsylvania":		12802503,
+	// 	"Illinois":			12801539,
+	// 	"Ohio":				11614373,
+	// }
+
+	// if pop, ok := statePopulations["Florida"]; ok {
+	// 	fmt.Println(pop)
+	// }
+
+	number := 50
+	guess := 30
+	if guess < 1 || returnTrue() || guess > 100 {
+		fmt.Println("The guess must be between 1 and 100")
+	}
+	if guess >= 1 && guess <= 100 {	
+		if guess < number {
+			fmt.Println("Too low!")
+		}
+		if guess > number {
+			fmt.Println("Too high!")
+		}
+		if guess == number {
+			fmt.Println("You got it!")
+		}
+		
+		fmt.Println(number <= guess, number >= guess, number != guess)
 	}
 
-	if pop, ok := statePopulations["Florida"]; ok {
-		fmt.Println(pop)
+	fmt.Println(!true, !false)
+
+	switch returnTrue() {
+	case true:
+		fmt.Println("Yolo")
+	case false:
+		fmt.Println("Swag")
+		
 	}
 }
