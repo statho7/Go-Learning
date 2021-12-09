5c319a02c9ca5dbe0808501d772ee44d6345feac
statho7 <andreas.stathop@gmail.com>
2021-12-09 23:22:40 +0000

    get request

--- src/github.com/statho7/defer_panic_recover/yolo.go
+++ src/github.com/statho7/defer_panic_recover/yolo.go
@@ -1,23 +1,45 @@
 package main
 
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	fmt.Println("start")
+// 	fmt.Println("middle")
+// 	fmt.Println("end")
+	
+// 	fmt.Println("~*~")
+
+// 	fmt.Println("start")
+// 	defer fmt.Println("middle")
+// 	fmt.Println("end")
+	
+// 	fmt.Println("~*~")
+
+// 	defer fmt.Println("start")
+// 	defer fmt.Println("middle")
+// 	defer fmt.Println("end")
+// }
+
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("start")
-	fmt.Println("middle")
-	fmt.Println("end")
-	
-	fmt.Println("~*~")
+	res, err := http.Get("http://www.google.com/robots.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	fmt.Println("start")
-	defer fmt.Println("middle")
-	fmt.Println("end")
-	
-	fmt.Println("~*~")
+	robots, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	defer fmt.Println("start")
-	defer fmt.Println("middle")
-	defer fmt.Println("end")
+	fmt.Printf("%s", robots)
 }
