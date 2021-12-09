5b009b5cbdb126053626fc50932cc445448312d5
statho7 <andreas.stathop@gmail.com>
2021-12-09 23:20:09 +0000

    defer all

--- src/github.com/statho7/defer_panic_recover/yolo.go
+++ src/github.com/statho7/defer_panic_recover/yolo.go
@@ -8,8 +8,16 @@ func main() {
 	fmt.Println("start")
 	fmt.Println("middle")
 	fmt.Println("end")
+	
+	fmt.Println("~*~")
 
 	fmt.Println("start")
 	defer fmt.Println("middle")
 	fmt.Println("end")
+	
+	fmt.Println("~*~")
+
+	defer fmt.Println("start")
+	defer fmt.Println("middle")
+	defer fmt.Println("end")
 }
