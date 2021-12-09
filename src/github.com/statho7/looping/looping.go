ca2a9e1b92db9683dd4984234db914d971e8fe59
statho7 <andreas.stathop@gmail.com>
2021-12-09 23:13:30 +0000

    loop through object

--- src/github.com/statho7/looping/looping.go
+++ src/github.com/statho7/looping/looping.go
@@ -75,4 +75,18 @@ func main()  {
 	// for k, v := range s {
 	// 	fmt.Println(k, v)
 	// }
+
+	statePopulations := map[string]int{
+		"California":		39250017,
+		"Texas":			27862596,
+		"Florida":			20612439,
+		"New York":			19745286,
+		"Pennsylvania":		12802503,
+		"Illinois":			12801539,
+		"Ohio":				11614373,
+	}
+
+	for k, v := range statePopulations {
+		fmt.Println(k, v)
+	}
 }
