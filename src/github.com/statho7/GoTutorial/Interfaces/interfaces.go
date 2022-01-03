2af42af9539e46f5f24030bff9af5bc013a0eb34
statho7 <andreas.stathop@gmail.com>
2022-01-03 15:00:46 +0000

    interfaces part 3

--- src/github.com/statho7/GoTutorial/Interfaces/interfaces.go
+++ src/github.com/statho7/GoTutorial/Interfaces/interfaces.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 )
 
@@ -20,21 +21,81 @@ import (
 // 	return n, err
 // }
 
-func main(){
-	myInt := IntCounter(0)
-	var inc Incrementer = &myInt
-	for i := 0; i < 10; i++ {
-		fmt.Println(inc.Increment())
+// func main(){
+// 	myInt := IntCounter(0)
+// 	var inc Incrementer = &myInt
+// 	for i := 0; i < 10; i++ {
+// 		fmt.Println(inc.Increment())
+// 	}
+// }
+
+// type Incrementer interface{
+// 	Increment() int
+// }
+
+// type IntCounter int
+
+// func (ic *IntCounter) Increment() int{
+// 	*ic++
+// 	return int(*ic)
+// }
+
+func main() {
+	var wc WriterCloser = NewBufferedWriterCloser()
+	wc.Write([]byte("Hello Youtube listeners, this is a test"))
+	wc.Close()
+}
+
+type Writer interface {
+	Write([]byte) (int, error)
+}
+
+type Closer interface {
+	Close() error
+}
+
+type WriterCloser interface {
+	Writer
+	Closer
+}
+
+type BufferedWriterCloser struct {
+	buffer *bytes.Buffer
+}
+
+func (bwc *BufferedWriterCloser) Write(data []byte) (int, error) {
+	n, err := bwc.buffer.Write(data)
+	if err != nil {
+		return 0, err
 	}
+
+	v := make([]byte, 8)
+	for bwc.buffer.Len() > 8 {
+		_, err := bwc.buffer.Read(v)
+		if err != nil {
+			return 0, err
+		}
+		_, err = fmt.Println(string(v))
+		if err != nil {
+			return 0, err
+		}
+	}
+	return n, nil
 }
 
-type Incrementer interface{
-	Increment() int
+func(bwc *BufferedWriterCloser) Close() error {
+	for bwc.buffer.Len() > 0 {
+		data := bwc.buffer.Next(8)
+		_, err := fmt.Println(string(data))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
-type IntCounter int
-
-func (ic *IntCounter) Increment() int{
-	*ic++
-	return int(*ic)
+func NewBufferedWriterCloser() *BufferedWriterCloser {
+	return &BufferedWriterCloser{
+		buffer: bytes.NewBuffer([]byte{}),
+	}
 }
