302995d717452356a495d57598456dd3403a6b9c
statho7 <andreas.stathop@gmail.com>
2022-01-03 14:29:11 +0000

    interfaces part 2

--- src/github.com/statho7/GoTutorial/Interfaces/interfaces.go
+++ src/github.com/statho7/GoTutorial/Interfaces/interfaces.go
@@ -4,18 +4,37 @@ import (
 	"fmt"
 )
 
-func main() {
-	var w Writer = ConsoleWriter{}
-	w.Write([]byte("Hello Go!"))
+// func main() {
+// 	var w Writer = ConsoleWriter{}
+// 	w.Write([]byte("Hello Go!"))
+// }
+
+// type Writer interface {
+// 	Write([]byte) (int, error)
+// }
+
+// type ConsoleWriter struct {}
+
+// func(cw ConsoleWriter) Write(data []byte) (int, error){
+// 	n, err := fmt.Println(string(data))
+// 	return n, err
+// }
+
+func main(){
+	myInt := IntCounter(0)
+	var inc Incrementer = &myInt
+	for i := 0; i < 10; i++ {
+		fmt.Println(inc.Increment())
+	}
 }
 
-type Writer interface {
-	Write([]byte) (int, error)
+type Incrementer interface{
+	Increment() int
 }
 
-type ConsoleWriter struct {}
+type IntCounter int
 
-func(cw ConsoleWriter) Write(data []byte) (int, error){
-	n, err := fmt.Println(string(data))
-	return n, err
+func (ic *IntCounter) Increment() int{
+	*ic++
+	return int(*ic)
 }
