b73886c6485d240334f6ff803417b50b808453f9
statho7 <andreas.stathop@gmail.com>
2022-01-03 15:25:16 +0000

    interfaces last part

--- src/github.com/statho7/GoTutorial/Interfaces/interfaces.go
+++ src/github.com/statho7/GoTutorial/Interfaces/interfaces.go
@@ -1,10 +1,10 @@
 package main
 
-import (
-	"bytes"
-	"fmt"
-	"io"
-)
+// import (
+// 	"bytes"
+// 	"fmt"
+// 	"io"
+// )
 
 // func main() {
 // 	var w Writer = ConsoleWriter{}
@@ -41,92 +41,108 @@ import (
 // 	return int(*ic)
 // }
 
+// func main() {
+// 	// // var wc WriterCloser = NewBufferedWriterCloser()
+// 	// // wc.Write([]byte("Hello Youtube listeners, this is a test"))
+// 	// // wc.Close()
+// 	// var wc WriterCloser = NewBufferedWriterCloser()
+// 	// wc.Write([]byte("Hello Youtube listeners, this is a test"))
+// 	// wc.Close()
+
+// 	// // bwc := wc.(*BufferedWriterCloser)
+// 	// // bwc := wc.(io.Reader)
+// 	// // fmt.Println(bwc)
+
+// 	// // r, ok := wc.(io.Reader)
+// 	// // r, ok := wc.(*BufferedWriterCloser)
+// 	// r, ok := wc.(BufferedWriterCloser)
+// 	// if ok {
+// 	// 	fmt.Println(r)
+// 	// } else {
+// 	// 	fmt.Println("Conversion failed")
+// 	// }
+
+// 	var myObj interface{} = NewBufferedWriterCloser()
+// 	if wc, ok := myObj.(WriterCloser); ok {
+// 		wc.Write([]byte("Hello Youtube listeners, this is a test"))
+// 		wc.Close()
+// 	}
+// 	r, ok := myObj.(io.Reader)
+// 	// r, ok := wc.(*BufferedWriterCloser)
+// 	// r, ok := wc.(BufferedWriterCloser)
+// 	if ok {
+// 		fmt.Println(r)
+// 	} else {
+// 		fmt.Println("Conversion failed")
+// 	}
+// }
+
+// type Writer interface {
+// 	Write([]byte) (int, error)
+// }
+
+// type Closer interface {
+// 	Close() error
+// }
+
+// type WriterCloser interface {
+// 	Writer
+// 	Closer
+// }
+
+// type BufferedWriterCloser struct {
+// 	buffer *bytes.Buffer
+// }
+
+// func (bwc *BufferedWriterCloser) Write(data []byte) (int, error) {
+// 	n, err := bwc.buffer.Write(data)
+// 	if err != nil {
+// 		return 0, err
+// 	}
+
+// 	v := make([]byte, 8)
+// 	for bwc.buffer.Len() > 8 {
+// 		_, err := bwc.buffer.Read(v)
+// 		if err != nil {
+// 			return 0, err
+// 		}
+// 		_, err = fmt.Println(string(v))
+// 		if err != nil {
+// 			return 0, err
+// 		}
+// 	}
+// 	return n, nil
+// }
+
+// func(bwc *BufferedWriterCloser) Close() error {
+// 	for bwc.buffer.Len() > 0 {
+// 		data := bwc.buffer.Next(8)
+// 		_, err := fmt.Println(string(data))
+// 		if err != nil {
+// 			return err
+// 		}
+// 	}
+// 	return nil
+// }
+
+// func NewBufferedWriterCloser() *BufferedWriterCloser {
+// 	return &BufferedWriterCloser{
+// 		buffer: bytes.NewBuffer([]byte{}),
+// 	}
+// }
+
+import (
+	"fmt"
+)
+
 func main() {
-	// // var wc WriterCloser = NewBufferedWriterCloser()
-	// // wc.Write([]byte("Hello Youtube listeners, this is a test"))
-	// // wc.Close()
-	// var wc WriterCloser = NewBufferedWriterCloser()
-	// wc.Write([]byte("Hello Youtube listeners, this is a test"))
-	// wc.Close()
-
-	// // bwc := wc.(*BufferedWriterCloser)
-	// // bwc := wc.(io.Reader)
-	// // fmt.Println(bwc)
-
-	// // r, ok := wc.(io.Reader)
-	// // r, ok := wc.(*BufferedWriterCloser)
-	// r, ok := wc.(BufferedWriterCloser)
-	// if ok {
-	// 	fmt.Println(r)
-	// } else {
-	// 	fmt.Println("Conversion failed")
-	// }
-
-	var myObj interface{} = NewBufferedWriterCloser()
-	if wc, ok := myObj.(WriterCloser); ok {
-		wc.Write([]byte("Hello Youtube listeners, this is a test"))
-		wc.Close()
-	}
-	r, ok := myObj.(io.Reader)
-	// r, ok := wc.(*BufferedWriterCloser)
-	// r, ok := wc.(BufferedWriterCloser)
-	if ok {
-		fmt.Println(r)
-	} else {
-		fmt.Println("Conversion failed")
-	}
-}
-
-type Writer interface {
-	Write([]byte) (int, error)
-}
-
-type Closer interface {
-	Close() error
-}
-
-type WriterCloser interface {
-	Writer
-	Closer
-}
-
-type BufferedWriterCloser struct {
-	buffer *bytes.Buffer
-}
-
-func (bwc *BufferedWriterCloser) Write(data []byte) (int, error) {
-	n, err := bwc.buffer.Write(data)
-	if err != nil {
-		return 0, err
-	}
-
-	v := make([]byte, 8)
-	for bwc.buffer.Len() > 8 {
-		_, err := bwc.buffer.Read(v)
-		if err != nil {
-			return 0, err
-		}
-		_, err = fmt.Println(string(v))
-		if err != nil {
-			return 0, err
-		}
-	}
-	return n, nil
-}
-
-func(bwc *BufferedWriterCloser) Close() error {
-	for bwc.buffer.Len() > 0 {
-		data := bwc.buffer.Next(8)
-		_, err := fmt.Println(string(data))
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func NewBufferedWriterCloser() *BufferedWriterCloser {
-	return &BufferedWriterCloser{
-		buffer: bytes.NewBuffer([]byte{}),
+	var i interface{} = true
+	switch i.(type) {
+	case int:
+		fmt.Println("i is an integer")
+	case string:
+		fmt.Println("i is a string")
+	default:
+		fmt.Println("I don't know what type of i is")
 	}
 }
