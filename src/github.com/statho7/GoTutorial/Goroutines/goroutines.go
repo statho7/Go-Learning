ad90b4db2877abfb400b0738b672258658ae3cdc
statho7 <andreas.stathop@gmail.com>
2022-01-03 15:59:53 +0000

    dafuq

--- src/github.com/statho7/GoTutorial/Goroutines/goroutines.go
+++ src/github.com/statho7/GoTutorial/Goroutines/goroutines.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"fmt"
-	"sync"
+	"runtime"
+	// "sync"
 )
 
 // var wg = sync.WaitGroup{}
@@ -23,24 +24,35 @@ import (
 // // 	fmt.Println("Hello")
 // // }
 
-var wg = sync.WaitGroup{}
-var counter = 0
+// var wg = sync.WaitGroup{}
+// var counter = 0
+// var m = sync.RWMutex{}
+
+// func main() {
+// 	runtime.GOMAXPROCS(1000)
+// 	for i := 0; i < 1000; i++ {
+// 		wg.Add(2)
+// 		m.RLock()
+// 		go sayHello()
+// 		m.Lock()
+// 		go increment()
+// 	}
+// 	wg.Wait()
+// }
+
+// func sayHello() {
+// 	fmt.Println("Hello #%v\n", counter)
+// 	m.RUnlock()
+// 	wg.Done()
+// }
+
+// func increment() {
+// 	counter++
+// 	m.Unlock()
+// 	wg.Done()
+// }
 
 func main() {
-	for i := 0; i < 10; i++ {
-		wg.Add(2)
-		go sayHello()
-		go increment()		
-	}
-	wg.Wait()
-}
-
-func sayHello() {
-	fmt.Println("Hello #v%\n", counter)
-	wg.Done()
-}
-
-func increment() {
-	counter++
-	wg.Done()
+	runtime.GOMAXPROCS(100)
+	fmt.Printf("Threads: %v", runtime.GOMAXPROCS(-1))
 }
