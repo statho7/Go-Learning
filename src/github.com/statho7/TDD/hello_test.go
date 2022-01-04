78ff7da133bf44aa9afb2e77049f858ccb27307d
statho7 <andreas.stathop@gmail.com>
2022-01-04 09:21:42 +0000

    test with go

--- src/github.com/statho7/TDD/hello_test.go
+++ src/github.com/statho7/TDD/hello_test.go
@@ -3,8 +3,10 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-    got := Hello()
-    want := "Hello, world"
+    // got := Hello()
+    got := Hello("Chris")
+    // want := "Hello, world"
+    want := "Hello, Chris"
 
     if got != want {
         t.Errorf("got %q want %q", got, want)
