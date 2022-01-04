3aea46fb01052c8aeac3c44c6a001d77eccf6a97
statho7 <andreas.stathop@gmail.com>
2022-01-04 10:12:20 +0000

    channels done

--- src/github.com/statho7/GoTutorial/Channels/channels.go
+++ src/github.com/statho7/GoTutorial/Channels/channels.go
@@ -86,6 +86,43 @@ package main
 // 	wg.Wait()
 // }
 
+// import (
+// 	"fmt"
+// 	"time"
+// )
+
+// const (
+// 	logInfo = "INFO"
+// 	logWarning = "WARNING"
+// 	logError = "ERROR"
+// )
+
+// type logEntry struct {
+// 	time time.Time
+// 	severity string
+// 	message string
+// }
+
+// var logCh = make(chan logEntry, 50)
+
+// func main() {
+// 	go logger()
+// 	defer func(){
+// 		close(logCh)
+// 	}()
+// 	logCh <- logEntry{time.Now(), logInfo, "App is starting"}
+	
+// 	logCh <- logEntry{time.Now(), logInfo, "App is shutting down"}
+// 	time.Sleep(100 * time.Millisecond)
+// }
+
+
+// func logger() {
+// 	for entry := range logCh {
+// 		fmt.Printf("%v - [%v]%v\n", entry.time.Format("2006-01-02T15:04:05"), entry.severity, entry.message)
+// 	}
+// }
+
 import (
 	"fmt"
 	"time"
@@ -104,6 +141,7 @@ type logEntry struct {
 }
 
 var logCh = make(chan logEntry, 50)
+var doneCh = make(chan struct{})
 
 func main() {
 	go logger()
@@ -112,13 +150,21 @@ func main() {
 	}()
 	logCh <- logEntry{time.Now(), logInfo, "App is starting"}
 	
-	logCh <- logEntry{time.Now(), logInfo, "App is starting"}
+	logCh <- logEntry{time.Now(), logInfo, "App is shutting down"}
 	time.Sleep(100 * time.Millisecond)
+	doneCh <- struct{}{}
 }
 
 
 func logger() {
-	for entry := range logCh {
-		fmt.Printf("%v - [%v]%v\n", entry.time.Format("2006-01-02T15:04:05"), entry.severity, entry.message)
+	for {
+		select {
+			case entry := <-logCh :
+				fmt.Printf("%v - [%v]%v\n", entry.time.Format("2006-01-02T15:04:05"), entry.severity, entry.message)
+			case <-doneCh:
+				break
+			// default:
+			// 	fmt.Println("Default")
+		}
 	}
 }
